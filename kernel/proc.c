ef8eb6cc808383e931c04b3f1fe72f4a3a8331b7
kth9245 <kth9245@korea.ac.kr>
2026-01-27 15:33:48 +0900

    Lab : System calls

--- kernel/proc.c
+++ kernel/proc.c
@@ -296,6 +296,9 @@ fork(void)
   }
   np->sz = p->sz;
 
+  // Copy mask trace setting
+  np->trace_mask = p->trace_mask;
+
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,11 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
+  // char secret[8];
+  
+  char *mem = sbrk(PGSIZE * 64);
 
+  char *addr = mem + 16 * PGSIZE + 32;
+  write(2, addr, 8);
   exit(1);
 }
